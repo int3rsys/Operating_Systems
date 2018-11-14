434f9cb7484c8ec13b03a6bd2b74f63f0f0cdf8e
Ariel Weiss <relweiss111@gmail.com>
2018-11-14 23:27:18 +0200

    Update hw1/linux_kernel/sys_set_process_capabilities.c

--- hw1/linux_kernel/sys_set_process_capabilities.c
+++ hw1/linux_kernel/sys_set_process_capabilities.c
@@ -5,8 +5,7 @@
 int sys_set_process_capabilities(pid_t pid, int new_level, int password){
 	if(pid < 0) return -ESRCH;
   struct task_struct* proc = find_task_by_pid(pid);
-  printk("[*] Setting a new capability for process %d, from level %d -> to level %d\n", pid, proc->privilege_level, new_level);
-	if(pid < 0 || proc == NULL){
+  if(pid < 0 || proc == NULL){
       return -ESRCH;
     }
     if(proc->is_policy_on == POLICY_OFF || password != 234123 || new_level < 0|| new_level > 2){
@@ -14,7 +13,8 @@ int sys_set_process_capabilities(pid_t pid, int new_level, int password){
     }
   // DO WE NEED TO CHECK IF CURRENT LEVEL == NEW_LEVEL ??
   // Needs to allocate memory here:
-
+  printk("[*] Setting a new capability for process %d, from level %d -> to level %d\n", pid, proc->privilege_level, new_level);
+	
   proc->privilege_level = new_level;
   return 0;
 }
