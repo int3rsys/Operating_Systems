3053240a90467fcb9b6245f949dc923cb8d2bf7a
Ilya Rozentsvaig <int3rsys@live.com>
2018-11-18 22:38:35 +0200

    Update sys_disable_policy.c

--- hw1/linux_kernel/sys_disable_policy.c
+++ hw1/linux_kernel/sys_disable_policy.c
@@ -17,20 +17,20 @@ int sys_disable_policy(pid_t pid, int password){
       return -EINVAL;
     }
 
-    printk("[*] Turning policy off for pid :%d \r\n", pid);
+    //printk("[*] Turning policy off for pid :%d \r\n", pid);
     // ** Here we delete the log **
     if(proc->log_array != NULL){
-      printk("__Clearing %d's logs__\r\n", proc->pid);
+      /*printk("__Clearing %d's logs__\r\n", proc->pid);
         for( ; i < proc->array_total_size ; i++){
           printk("\t[*] deleting log with req_level %d, proc level %d, created in %d\n", proc->log_array[i].syscall_req_level,proc->log_array[i].proc_level,proc->log_array[i].time);
-        }
+        }*/
     }
     kfree(proc->log_array);
     proc->log_array = NULL;
     proc->is_policy_on = POLICY_OFF;
 	proc->curr_size = 0;
 	proc->privilege_level = 2;
-    printk("[*] Policy for %d changed to: %d \r\n", pid, proc->is_policy_on);
+    //printk("[*] Policy for %d changed to: %d \r\n", pid, proc->is_policy_on);
 	
 	
     return 0;
